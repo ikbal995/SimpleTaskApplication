9264974e50d504fea80873fa9a58359033c3a252
ikiMulalic123 <62341655+ikiMulalic123@users.noreply.github.com>
2025-09-18 18:58:21 +0200

    Initial commit

---
+++ app/src/main/java/ikbal/mulalic/simpletaskapplication/MainActivity.kt
@@ -0,0 +1,49 @@
+package ikbal.mulalic.simpletaskapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import ikbal.mulalic.simpletaskapplication.ui.theme.SimpleTaskApplicationTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimpleTaskApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SimpleTaskApplicationTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/ikbal/mulalic/simpletaskapplication/SimpleApplication.kt
@@ -0,0 +1,8 @@
+package ikbal.mulalic.simpletaskapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SimpleApplication : Application() {
+}
